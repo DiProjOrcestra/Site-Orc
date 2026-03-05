a9e83b462f7158e36bb98acadc16d7ba4f5f353e
Gotc2607 <giovanicoelho2005@gmail.com>
2026-03-04 23:39:46 -0300

    fix: Concertando os css dos arquivos QuemSomos.jsx e do Beneficios.jsx

--- src/components/PageServicos/QuemSomos.jsx
+++ src/components/PageServicos/QuemSomos.jsx
@@ -12,12 +12,13 @@ const SobreInstitucional = () => {
       <div className="institucional-container">
         <div className="institucional-block">
           <h2>Quem somos nós?</h2>
-          <p>A Orc'estra Game Studio é a Empresa Júnior do curso de Engenharia de Software da Universidade de Brasília, campus Gama (FGA)...</p>
+          <p>A Orc’estra Gamificação (ou Orc para os mais íntimos) é a Empresa Júnior de Engenharia de Software da Universidade de Brasília e a primeira a trabalhar com foco em Gamificação no Brasil, sempre procurando transformar o comportamento das pessoas por meio da Gamificação.</p>
+          <p>Trazemos mais motivação e engajamento para nossos parceiros com soluções gamificadas únicas, com o auxílio de metodologias inovadoras consolidadas no mercado.</p>
         </div>
 
         <div className="institucional-block">
           <h2>Por que gamificação?</h2>
-          <p>A gamificação é a aplicação de elementos e conceitos de jogos no mundo real...</p>
+          <p>A Gamificação é a aplicação de elementos e conceitos de jogos no mundo real com o intuito de mudar o comportamento das pessoas.Para nós, a Gamificação é muito mais que uma técnica, é uma forma de motivar e engajar pessoas ressignificando suas vidas e transformando obrigações em desafios a serem superados para que o trabalho seja visto como algo prazeroso e divertido de se fazer.</p>
         </div>
 
         <button onClick={() => document.getElementById('contato').scrollIntoView({ behavior: 'smooth' })}>

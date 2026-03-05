3eaf0899f96bcca2f5c4a713a6acced6e33dc66d
Paulodemczuk <paulodemczuk@gmail.com>
2026-03-05 12:01:41 -0300

    refactor: Reorganizado codigo em um novo componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,8 @@
-import "./App.css"
+import ImpactIndicators from "./ImpactIndicators"
 
 function App() {
   return (
-    <div className="parallax">
-      <div className="overlay">
-        <div className="metrics-container">
-          
-          <div className="metric-card">
-            <h2>+35</h2>
-            <p>Projetos executados</p>
-          </div>
-          
-          <div className="metric-card">
-            <h2>+8</h2>
-            <p>Anos de atuação</p>
-          </div>
-
-          <div className="metric-card">
-            <h2>1º</h2>
-            <p>EJ de Gamificação</p>
-          </div>
-
-          <div className="metric-card">
-            <h2>9</h2>
-            <p>Média de NPS</p>
-          </div>
-
-          <div className="metric-card">
-            <h2>3x</h2>
-            <p>EJ Impacto</p>
-          </div>
-
-        </div>
-      </div>
-    </div>
+    <ImpactIndicators></ImpactIndicators>
   )
 }
 

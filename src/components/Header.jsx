c1b7634bc3163c3e7ae0647f4ebd30c7e398ac6e
pedro <pedrinhodelcastilo@gmail.com>
2026-03-05 15:00:36 -0300

    feat: adicionado menu no serviços

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,5 @@
 import './Header.css';
-// Importe a logo (escolha uma das opções)
-import logo from '../assets/logo.png'; // ou logo.png
+import logo from '../assets/logo.png'; 
 
 const Header = () => {
   return (
@@ -14,7 +13,16 @@ const Header = () => {
        
         <ul>
           <li><a href="/">Início</a></li>
-          <li><a href="/servicos">Serviços</a></li>
+          <li className='dropdown'>
+            <a href="/servicos">Serviços</a>
+            <ul className='dropdown-menu'>
+              <li><a href="/servicos/consultoria-gamificada">Consultoria Gamificada</a></li>
+              <li><a href="/servicos/desenvolvimento-gamificado">Desenvolvimento Gamificado</a></li>
+              <li><a href="/concepção-de-software">Concepção de Software</a></li>
+              <li><a href="/servicos/treinamento-gamificado">Treinamento Gamificado</a></li>
+              <li><a href="/design-de-gamificacao">Design de Gamificação</a></li>
+            </ul>
+          </li>
           <li><a href="/portfolio">Portfólio</a></li>
           <li><a href="/contato">Contato</a></li>
           <li><a href="/quem-somos">Quem Somos</a></li>

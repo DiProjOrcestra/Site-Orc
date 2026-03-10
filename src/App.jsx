cc4b2492a078fd59edfe823d79590bd97c56ccd8
VinyCrz <vinicius.crz456@gmail.com>
2026-03-10 09:53:59 -0300

    Feature/visualizar indicadores de impacto (#3)
    * Atualizações do site
    
    * feat: adicionado conteudo aos containers de indicadores de impacto
    
    * refactor: Reorganizado codigo em um novo componente
    
    * refactor: Imagem movida para public
    
    * fix: banner agora ocupa toda a largura da tela
    
    * feat: implementada responsividade para telas menores (celulares)
    
    * fix: cor de fundo
    
    * fix: cor alterada com transparencia
    
    ---------
    
    Co-authored-by: Paulodemczuk <paulodemczuk@gmail.com>
    Co-authored-by: Natan França <natan.j.franca@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import React from 'react';
-import PageServicos from './pages/PageServicos';
-import { SERVICOS } from './data/services'; // Importe seus dados
+import ImpactIndicators from "./ImpactIndicators"
 
 function App() {
-  // Simulamos o que o useParams faria, pegando um serviço direto do objeto
-  const servicoParaTeste = SERVICOS["design-de-gamificacao"]; // Substitua pela chave do serviço que deseja testar
-
   return (
-    <div className="App">
-      {/* Ajustamos o componente para aceitar os dados diretamente via props 
-          em vez de buscar pelo ID da URL
-      */}
-      <PageServicos DadosManuais={servicoParaTeste} />
-    </div>
-  );
+    <ImpactIndicators></ImpactIndicators>
+  )
 }
 
-export default App;
+export default App

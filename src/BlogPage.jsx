bfabaeaf54cd092b535f1741b2e8bd64fe7f519a
VinyCrz <vinicius.crz456@gmail.com>
2026-03-12 22:52:22 -0300

    feat: adiciona texto, imagem e ajustes de estilo na seção do blog

--- src/BlogPage.jsx
+++ src/BlogPage.jsx
@@ -1,9 +1,31 @@
+import "./BlogPage.css";
+import blogframe from "../public/frame-blog.svg";
+
 function BlogPage() {
-    return ( 
-        <>
-        
-        </>
-     );
+  return (
+    <section className="blog-c">
+      <div className="blog-content">
+        <a href="/blog" className="blog-tag">
+          Blog
+        </a>
+
+        <h1 className="blog-title">
+          Além de empresa,
+          <br />
+          também é informação!
+        </h1>
+
+        <p className="blog-text">
+          Conteúdos sobre inovação, tecnologia e muito mais você só aprende
+          aqui, no Blog da Orc’estra
+        </p>
+      </div>
+
+      <div className="blog-frame-image">
+        <img src={blogframe} alt="Ilustração do desenhoo" />
+      </div>
+    </section>
+  );
 }
 
 export default BlogPage;

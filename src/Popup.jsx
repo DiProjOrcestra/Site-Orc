bd5fc8d5e6c6798fbaabb8be02adc25e7e6e8bc3
pedro <pedrinhodelcastilo@gmail.com>
2026-03-17 17:35:23 -0300

    fix: ajustando algumas responsividades

--- src/Popup.jsx
+++ src/Popup.jsx
@@ -1,29 +1,32 @@
 import React, { useState, useEffect } from 'react';
-import './Popup.css'; // seus estilos customizados (opcional)
+import './Popup.css';
 import ContactForm from './form';
 
 const PopupAfterThreeMinutes = () => {
   const [showPopup, setShowPopup] = useState(false);
 
   useEffect(() => {
-    // Define o timer para 3 minutos (180000 ms)
     const timer = setTimeout(() => {
       setShowPopup(true);
-    }, 5000); // 3 minutos = 3 * 60 * 1000
-
-    // Limpeza do timer quando o componente for desmontado
+    }, 30000); // 30 segundos
     return () => clearTimeout(timer);
-  }, []); // Array vazio garante que o efeito rode apenas uma vez
+  }, []);
 
   const handleClosePopup = () => {
     setShowPopup(false);
   };
 
+  // Impede que o clique no conteúdo feche o pop-up
+  const handleContentClick = (e) => {
+    e.stopPropagation();
+  };
+
   return (
     <div>
       {showPopup && (
-        <div className="popup-overlay">
-          <div className="popup-content">
+        <div className="popup-overlay" onClick={handleClosePopup}>
+          <div className="popup-content" onClick={handleContentClick}>
+            <h2 className='title'>Entre em Contato</h2>
             <button className="close-button" onClick={handleClosePopup}>
               <img src="./assets/close.png" alt="fechar" />
             </button>
